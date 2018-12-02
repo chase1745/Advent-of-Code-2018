70f227254ab7d2a7c4b7ababf4cdc2cdca92cac6
Chase McDermott <chasemcd1745@tamu.edu>
2018-12-01 21:55:44 -0600

    Helper file.

---
+++ adventHelper.py
@@ -0,0 +1,8 @@
+class FileReader():
+    def __init__(self, day, problem):
+        self.filename = 'day{}-{}.txt'.format(day, problem)
+
+    def getlines(self):
+        with open(self.filename, 'r') as f:
+            for line in f:
+                yield line
